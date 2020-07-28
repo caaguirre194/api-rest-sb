e053f58ca324efe605f7df7bd90d9f54f9c1d8ed
caaguirre194 <caaguirre194@gmail.com>
2020-07-27 19:53:57 -0500

    JWT image is added for README

--- api/api/src/main/java/com/caaguirre/api/repository/IUserRepository.java
+++ api/api/src/main/java/com/caaguirre/api/repository/IUserRepository.java
@@ -1,10 +1,7 @@
 package com.caaguirre.api.repository;
 
 import com.caaguirre.api.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
-import java.util.Optional;
 
 public interface IUserRepository {
 
